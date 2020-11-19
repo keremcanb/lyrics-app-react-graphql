ede6d54984506b6aec31aeeadad54cec24526278
keremcanb <keremcan@gmail.com>
2020-11-19 23:16:54 +0300

    lyric create mutation

--- client/components/LyricCreate.js
+++ client/components/LyricCreate.js
@@ -1,15 +1,38 @@
-import React from 'react';
-// import gql from 'graphql-tag';
-// import { graphql } from 'react-apollo';
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-const LyricCreate = () => {
+const LyricCreate = ({ mutate, songId }) => {
+  const [content, setContent] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    mutate({
+      variables: {
+        content,
+        songId,
+      },
+    }).then(() => setContent(''));
+  };
+
   return (
-    <div>
-      <h3>Lyric Create</h3>
-    </div>
+    <form onSubmit={submitHandler}>
+      <label>Add a Lyric</label>
+      <input value={content} onChange={(e) => setContent(e.target.value)} />
+    </form>
   );
 };
 
-export default LyricCreate;
+const mutation = gql`
+  mutation AddLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`;
 
-// export default graphql(mutation)(LyricCreate);
+export default graphql(mutation)(LyricCreate);
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
-// import LyricCreate from './LyricCreate';
+import LyricCreate from './LyricCreate';
 // import LyricList from './LyricList';
 
-const SongDetail = ({ data: { song } }) => {
+const SongDetail = ({ data: { song }, params }) => {
   return song ? (
     <div>
       <Link to='/'>Back</Link>
       <h3>{song.title}</h3>
+      {/* pass song id to lyric create comp */}
+      <LyricCreate songId={params.id} />
     </div>
   ) : (
     <div></div>
