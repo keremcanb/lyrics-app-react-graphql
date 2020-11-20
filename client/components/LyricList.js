774aff646e9fa6052e64bc8b097a0d28f23b00da
keremcanb <keremcan@gmail.com>
2020-11-20 21:40:44 +0300

    thumbs working

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -2,24 +2,37 @@ import React from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const LyricList = ({ lyrics }) => {
+const LyricList = ({ lyrics, mutate }) => {
   const onLike = (id) => {
-    console.log(id);
+    mutate({
+      variables: { id },
+    });
   };
 
   return (
     <ul className='collection'>
-      {lyrics.map(({ id, content }) => (
+      {lyrics.map(({ id, content, likes }) => (
         <li key={id} className='collection-item'>
           {content}
-          <i className='material-icons' onClick={() => onLike(id)}>
-            thumb_up
-          </i>
+          <div className='vote-box'>
+            <i className='material-icons' onClick={() => onLike(id)}>
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       ))}
     </ul>
   );
 };
 
-export default LyricList;
-// export default graphql(mutation)(LyricList);
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
